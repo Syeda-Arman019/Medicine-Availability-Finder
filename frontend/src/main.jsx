483cbf5792186a2edee0691631b7181aa69aba24
Syeda-Arman019 <syedaarman11@gmail.com>
2026-08-31 00:15:15 +0500

    add netlify.toml

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+import "./index.css?v=8"
+
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
 
